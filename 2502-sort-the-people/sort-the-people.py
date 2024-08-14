eb9e42c3ae256e24ca2cc174ff0f2d4244faad86
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-08-14 22:03:54 +0300

    Time: 99 ms (77.23%) | Memory: 17 MB (64.26%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,12 +1,5 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        while True:
-            flag = False
-            for j in range(len(names) - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
-                    flag = True
-            if not flag:
-                break
-        return names
+        dic = dict(zip(heights,names))
+        lst_h = sorted(heights, reverse=True)
+        return (dic[h] for h in lst_h)
