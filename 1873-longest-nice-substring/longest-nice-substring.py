087a8b3ca439f94df9e444a801158680735453ae
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-09-08 19:13:12 +0300

    Time: 38 ms (77.96%) | Memory: 16.3 MB (97.09%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,20 +1,11 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        maxi = ""
-        for i in range(len(s)):
-            low = set()
-            up = set()
-            for j in range(i, len(s)):
-                char = s[j]
-                if char.islower():
-                    low.add(char)
-                else:
-                    up.add(char)
-                
-                if (char.isupper() and char.lower() not in low) or (char.islower() and char.upper() not in up):
-                    continue
-                if len(low) == len(up) and all(c.upper() in up for c in low):
-                    if len(maxi) < len(s[i:j+1]):
-                        maxi = s[i:j+1]
-        
-        return maxi
+        if len(s) == 1:
+            return ""
+        for i, val in enumerate(s):
+            if val.lower() in s and val.upper() in s:
+                continue
+            left = self.longestNiceSubstring(s[:i])
+            right = self.longestNiceSubstring(s[i+1:])
+            return (left if len(left) >= len(right) else right)
+        return s
