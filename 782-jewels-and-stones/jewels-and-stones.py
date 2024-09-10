0ce245045f4b5a567f2ea5861d6c400a6a00c004
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-09-10 22:20:08 +0300

    Time: 41 ms (16.32%) | Memory: 16.5 MB (24.05%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,4 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
         seen = set(jewels)
-        return sum(i in seen for i in stones)
+        for i in stones:
+            if i in seen:
+                count += 1
+        return count
