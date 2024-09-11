a2f27c49e3ff7c69846a23b47561225d548f21fb
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-09-11 22:48:48 +0300

    Time: 457 ms (53.75%) | Memory: 19.5 MB (80.52%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 0:
                 zero += 1 
-            while zero > 1 and i >= j:
+            while zero > 1:
                 if nums[j] == 0:
                     zero -= 1
                 j += 1
