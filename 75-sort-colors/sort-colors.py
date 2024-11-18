f21a089c066d843f2502eb02848f2424f16cbbc7
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-11-18 23:19:17 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (64.65%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -8,8 +8,8 @@ class Solution:
         while p <= j:
             if nums[p] == 0:
                 nums[p], nums[i] = nums[i], nums[p]
-                i += 1
                 p += 1
+                i += 1
             elif nums[p] == 2:
                 nums[p], nums[j] = nums[j], nums[p]
                 j -= 1
