54badb572bf29602700bd99b84a57c5e5d71dfae
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-11-19 11:00:01 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (23.60%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,22 +1,20 @@
 from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dic1 = Counter(nums1)
-        dic2 = Counter(nums2)
-        N = []
-        # for val in nums1:
-        #     if val in dic1:
-        #         dic1[val] += 1
-        #     else:
-        #         dic1[val] = 1
-
-        # for val2 in nums2:
-        #     if val2 in dic2:
-        #         dic2[val2] += 1
-        #     else:
-        #         dic2[val2] = 1
-
-        for i in dic1:
-            if i in dic2:
-                N.extend([i] * min(dic1[i], dic2[i]))
-        return N   
+        # brute force
+        # min_num = min(nums1, nums2)
+        # max_num = max(nums1, nums2)
+        # lst = []
+        # for i in range(len(max_num)):
+        #     if max_num[i] in min_num:
+        #         lst.append(max_num[i])
+        #         min_num.remove(max_num[i])
+        # return lst
+        
+        count = Counter (nums2)
+        lst = []
+        for i, val in enumerate(nums1):
+            if count[val]:
+                lst.append(val)
+                count[val] -= 1
+        return lst
