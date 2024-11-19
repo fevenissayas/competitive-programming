05fe2a83f84cb4f5f16161c7e7cc3fdccca1595f
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-11-19 09:25:54 +0300

    Time: 3 ms (71.67%) | Memory: 16.6 MB (88.36%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,7 +1,28 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        dic = {}
-        for i, val in enumerate (sorted(nums)):
-            if val not in dic:
-                dic[val] = i
-        return [dic[val] for val in nums]
+        # [1,2,2,3,8]
+        # [0,1,2,3,4]
+#optimized
+
+        numss = sorted(nums)
+        s = {}
+        for i in range(len(nums)):
+            if numss[i] not in s:
+                s[numss[i]] = i
+        return [s[num] for num in nums]
+# time complexity: O(nlogn)
+#SPACE COMPLEXITY: O(n)
+
+#brute force
+        # lst = []
+        # for i in range(len(nums)): O(N)
+        #     v = 0
+        #     for j in range(len(nums)):O(N)
+        #         if nums[i] > nums[j]:
+        #             v += 1
+        #     lst.append(v)
+        # return lst
+    
+# time complexity == O(n2)
+# space complexity == O(n)
+
