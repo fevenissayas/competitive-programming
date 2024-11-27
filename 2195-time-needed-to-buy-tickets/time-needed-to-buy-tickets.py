cd4fb1a7f3a7812f630d64c880108b3b2b116b4e
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2024-11-27 19:21:50 +0300

    Time: 12 ms (17.21%) | Memory: 17.2 MB (9.58%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,13 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        q = 0
-        while True:
-            if tickets[k] == 0:    
-                break
-            for i in range(len(tickets)):
-                if tickets[k] == 0:    
-                    break
-                if tickets[i] > 0:
-                    tickets[i] -= 1
-                    q += 1
-        return q
+        queue = deque(range(len(tickets)))
+        val = 0
+        while tickets[k]:
+            i = queue.popleft()
+            if tickets[i] > 0:
+                tickets[i] -= 1
+                val += 1
+            queue.append(i)
+            
+        return val
