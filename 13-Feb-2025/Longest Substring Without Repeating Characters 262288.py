a608711fb6aa0a58047a3ec9703c039ff45338c8
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-02-13 11:27:49 +0300

    Created 'Longest Substring Without Repeating Characters' by A2SV Hub

---
+++ 13-Feb-2025/Longest Substring Without Repeating Characters 262288.py
@@ -0,0 +1,17 @@
+# Problem: Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+
+        left = 0
+        max_leng = 0
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_leng = max(max_leng, right-left+1)
+        
+        return max_leng
+
