b5b871a34d5c8bcc3d2ea9f5931fb0e634b55c97
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-02-23 20:03:57 +0300

    Created 'Shuffle the Array' by A2SV Hub

---
+++ 23-Feb-2025/Shuffle the Array 281161.py
@@ -0,0 +1,14 @@
+# Problem: Shuffle the Array - https://leetcode.com/problems/shuffle-the-array/
+
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        answer = []
+        for i in range(n):
+            answer.append(nums[i])
+            answer.append(nums[i + n])
+        return answer
