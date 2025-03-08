01a66ca776c73286d1ef5c92a195f69303e5c48c
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-08 21:47:32 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 08-Mar-2025/F - Covered Points Count 298153.py
@@ -0,0 +1,25 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+t = int(input())
+
+from collections import defaultdict
+prefix = defaultdict(int)
+for _ in range(t):
+    ranges = list(map(int, input().split()))
+    prefix[ranges[0]] += 1
+    prefix[ranges[1] + 1] -= 1
+
+key = sorted(prefix.keys())
+
+ans = defaultdict(int)
+running_sum = 0
+
+for i in range(len(key) - 1):
+    running_sum += prefix[key[i]]
+    ans[running_sum] += (key[i+1] - key[i])
+
+res = []
+for i in range(1, t+1):
+    res.append(ans[i] if i in ans else 0)
+
+print(*res)
