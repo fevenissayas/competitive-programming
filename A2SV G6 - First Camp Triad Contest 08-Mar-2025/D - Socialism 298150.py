92561168a098d5ba140c108275df023a983d143e
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-08 21:43:22 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 08-Mar-2025/D - Socialism 298150.py
@@ -0,0 +1,24 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, x = map(int, input().split())
+    a = sorted(map(int, input().split()))
+    riches = 0
+    money = 0
+ 
+ 
+    for i in range(n):
+        if a[i] >= x:
+            riches += 1
+            money += a[i] - x
+        
+    for i in range(n - 1,-1,-1):
+        if a[i] < x:
+            money -= (x - a[i])
+            if money < 0:
+                break
+            else:
+                riches += 1
+ 
+    print(riches)
