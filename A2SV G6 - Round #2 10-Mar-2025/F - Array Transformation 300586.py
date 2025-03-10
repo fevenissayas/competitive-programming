bad35450b0142231c1fd004a6f1d0155410ff1dc
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-10 20:01:33 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 10-Mar-2025/F - Array Transformation 300586.py
@@ -0,0 +1,21 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import defaultdict
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    count = defaultdict(int)
+    dic = defaultdict(int)
+    for num in nums:
+        count[num] += 1
+        current_count = count[num]
+        dic[current_count] += 1
+
+    maxx = 0
+    for key , val in dic.items():
+        maxx = max(key * val , maxx)
+
+    print(n - maxx)
