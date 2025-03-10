a233a46730d8609ec8e2f95785b79a25ce6668ce
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-10 19:55:37 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 10-Mar-2025/F - Binary Substrings with Exactly k Ones 300574.py
@@ -0,0 +1,17 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+nums = list(map(int, list(input())))
+
+from collections import defaultdict
+count = defaultdict(int)
+ans = 0
+summ = 0
+count[0] = 1
+
+for val in nums:
+    summ += val
+    ans += count[summ-k]
+    count[summ] += 1 
+
+print(ans)
