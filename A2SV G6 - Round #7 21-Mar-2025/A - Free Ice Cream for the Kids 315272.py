7136e45dd63ce59e2ece919a51b90db49cce557b
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-21 22:52:44 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 21-Mar-2025/A - Free Ice Cream for the Kids 315272.py
@@ -0,0 +1,19 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n , m = map(int, input().split())
+q = []
+for _ in range(n):
+    q.append(list(input().split()))
+
+ans = m
+count = 0
+for e, num in q:
+    if e == "+":
+        ans += int(num)
+    else:
+        if ans < int(num):
+            count += 1
+        else:
+            ans -= int(num)
+
+print(ans, count)
