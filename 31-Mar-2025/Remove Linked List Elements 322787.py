13424c5d62e5a3e0f519821a7e456dd8ad43360a
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-03-31 21:32:09 +0300

    Created 'Remove Linked List Elements' by A2SV Hub

---
+++ 31-Mar-2025/Remove Linked List Elements 322787.py
@@ -0,0 +1,17 @@
+# Problem: Remove Linked List Elements - https://leetcode.com/problems/remove-linked-list-elements/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return None
+            
+        if head.val != val:
+            head.next = self.removeElements(head.next, val)
+            return head
+        else:
+            return self.removeElements(head.next, val)
