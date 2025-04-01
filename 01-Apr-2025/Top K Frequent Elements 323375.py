8efd0a8789de160af91bfdfeb23eb33a4d4ec81c
Feven Issayas <165965873+fevenissayas@users.noreply.github.com>
2025-04-01 18:53:45 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 01-Apr-2025/Top K Frequent Elements 323375.py
@@ -0,0 +1,6 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums).most_common(k)
+        return [x[0] for x in count]
